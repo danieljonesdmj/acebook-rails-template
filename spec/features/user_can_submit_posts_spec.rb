fea80c8c88cf28c6b8c188389d9810e61d77e88f
Bibi Collins <bibicollins94@googlemail.com>
2018-08-02 12:33:01 +0100

    Added Controller helpers to rails_helper, so can now find Warden::Proxy

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
+    visit "/users/sign_in"
+    fill_in "email", with: 'hello1@gmail.com'
+    fill_in "password", with: 'hello1'
     visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello, coppers!\bDid you miss me?"
