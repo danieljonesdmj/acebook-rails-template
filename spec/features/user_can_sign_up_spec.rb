0354cfc00167392a71205630a4179390cda1a0bb
Bibi Collins <bibicollins94@googlemail.com>
2018-08-10 00:03:33 +0100

    Added feature tests for sign-up with unmatched password & password confirmation

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -5,9 +5,24 @@ require 'helpers/users_helper_spec'
 
 RSpec.feature 'Registration', type: :feature do
   scenario 'Can register with correct details and be directed to posts page' do
+    visit '/users/sign_up'
+    expect(page).to have_content('Username')
+    fill_in 'user_username', with: 'test'
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: 'test123'
+    fill_in 'user_password_confirmation', with: 'test123'
+    click_button 'Sign up'
+    expect(page).to have_content('Welcome! You have signed up successfully.')
   end
   scenario 'Cannot register if passwords don\'t match' do
-  end 
+    visit '/users/sign_up'
+    expect(page).to have_content('Username')
+    fill_in 'user_username', with: 'test'
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: 'test123'
+    fill_in 'user_password_confirmation', with: 'test567'
+    click_button 'Sign up'
+    expect(page).to have_content('Password confirmation doesn\'t match Password')
+  end
 
   end
-end
