6f97561fa6984cb1cafbcf802b78ba83521a561b
Bibi Collins <bibicollins94@googlemail.com>
2018-08-09 19:14:30 +0100

    Added structure for tests

---
+++ spec/features/user_can_log_in_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'helpers/users_helper_spec'
+
+RSpec.feature 'Sign_in', type: :feature do
+  scenario 'Can Login with correct email and password' do
+
+  end
+  scenario 'Cannot Login without correct email or password' do
+  end
+  scenario 'Redirect to sign_in page if not signed in' do
+  end
+end
---
+++ spec/features/user_can_sign_up_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'helpers/users_helper_spec'
+
+RSpec.feature 'Registration', type: :feature do
+  scenario 'Can register with correct details and be directed to posts page' do
+  end
+  scenario 'Cannot register if passwords don\'t match' do
+  end 
+
+  end
+end
